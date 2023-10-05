b6b0159274d1ff49310d7316602751cfcafaf6de
RomelDonovan <romeldonovan@live.ca>
2023-10-05 19:47:49 -0400

    Add mocks data in navbar and pass to topiclist

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,11 +3,12 @@ import TopicList from 'components/TopicList';
 import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
+  const topics = props.topics
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList />
+      <TopicList topics={topics} />
       <FavBadge />
     </div>
   )
