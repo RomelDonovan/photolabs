b1d4def561f3587067df4192f90df40a2f911762
RomelDonovan <romeldonovan@live.ca>
2023-10-05 16:56:40 -0400

    Add <span> to props.title for css hover

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -5,7 +5,9 @@ import "../styles/TopicListItem.scss";
 const TopicListItem = (props) => {
   return (
     <div className="topic-list__item">
-      {props.title}
+      <span>
+        {props.title}
+      </span>
     </div>
   );
 };
