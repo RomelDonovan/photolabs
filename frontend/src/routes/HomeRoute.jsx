b9b33c2c3ef2fb6d858b7c3331f927bd4f6341dd
RomelDonovan <romeldonovan@live.ca>
2023-10-05 19:28:13 -0400

    Add HomeRoute mock data to Navbar and Photolist

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,11 +3,13 @@ import PhotoList from '../components/PhotoList';
 import TopNavigationBar from '../components/TopNavigationBar';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
+  const photos = props.photos
+  const topics = props.topics
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar topics={topics}/>
+      <PhotoList photos={photos}/>
     </div>
   );
 };
