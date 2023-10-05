84b91881172f743fe63042df2ed9e1f5bf1a03ad
RomelDonovan <romeldonovan@live.ca>
2023-10-05 15:41:44 -0400

    Import and add TopicList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
+import TopicList from 'components/TopicList';
 import './App.scss';
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const photos = Array(3).fill(sampleDataForPhotoListItem);
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {photos.map((photo, id) => (
-        <PhotoListItem
-          key={id}
-          id={sampleDataForPhotoListItem.id}
-          location={sampleDataForPhotoListItem.location}
-          imageSource={sampleDataForPhotoListItem.imageSource}
-          username={sampleDataForPhotoListItem.username}
-          profile={sampleDataForPhotoListItem.profile}
-        />
-      ))}
+      <TopicList />
+      <PhotoList />
     </div>
   );
 };
