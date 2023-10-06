125dc1b52093ab2bbd15754c37cbf15db5d0dcf7
RomelDonovan <romeldonovan@live.ca>
2023-10-06 10:50:50 -0400

    Pass state to navbar and change state to photolist

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import PhotoList from '../components/PhotoList';
 import TopNavigationBar from '../components/TopNavigationBar';
 import '../styles/HomeRoute.scss';
@@ -6,10 +7,11 @@ import '../styles/HomeRoute.scss';
 const HomeRoute = (props) => {
   const photos = props.photos
   const topics = props.topics
+  const [fav, setFav] = useState(false);
   return (
     <div className="home-route">
-      <TopNavigationBar topics={topics}/>
-      <PhotoList photos={photos}/>
+      <TopNavigationBar topics={topics} fav={fav}/>
+      <PhotoList photos={photos} setFav={setFav}/>
     </div>
   );
 };
