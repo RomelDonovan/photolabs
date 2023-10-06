61d48dc4d962dceb73a97ddfb4276534f471b051
RomelDonovan <romeldonovan@live.ca>
2023-10-06 16:47:13 -0400

    Add useState  to modal

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,15 +3,21 @@ import { useState } from 'react';
 import PhotoList from '../components/PhotoList';
 import TopNavigationBar from '../components/TopNavigationBar';
 import '../styles/HomeRoute.scss';
+import PhotoDetailsModal from './PhotoDetailsModal';
 
 const HomeRoute = (props) => {
   const photos = props.photos
   const topics = props.topics
   const [fav, setFav] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <div className="home-route">
-      <TopNavigationBar topics={topics} fav={fav}/>
-      <PhotoList photos={photos} setFav={setFav}/>
+      <TopNavigationBar topics={topics} fav={fav} />
+      <PhotoList photos={photos}
+        setFav={setFav}
+        setModalVisible={setModalVisible} />
+        {modalVisible && <PhotoDetailsModal/>}
     </div>
   );
 };
