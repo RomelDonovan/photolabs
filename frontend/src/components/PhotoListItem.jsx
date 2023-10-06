0afe0b8cae6feb20a2da01678e9593d44dc50b06
RomelDonovan <romeldonovan@live.ca>
2023-10-06 16:48:38 -0400

    Create blank modal when users click a photo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,10 +3,13 @@ import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
+  const handleClick = () => {
+    props.setModalVisible(true);
+  }
   return (
     <div className="photo-list__item">
       <PhotoFavButton setFav={props.setFav} />
-      <img src={props.urls.regular} alt="" className="photo-list__image" />
+      <img src={props.urls.regular} alt="" className="photo-list__image" onClick={handleClick}/>
       <div className="photo-list__user-details">
       <img src={props.user.profile} alt="" className="photo-list__user-profile" />
       <p className="photo-list__user-info">
