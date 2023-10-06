279688e7b062a385ad332a6d70e48ce15f966f4e
RomelDonovan <romeldonovan@live.ca>
2023-10-06 10:58:03 -0400

    Add setFav to handleClick

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,9 +3,12 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
   const [selected, setSelected] = useState(false);
-  const handleClick = () => setSelected(!selected);
+  const handleClick = () => {
+    setSelected(!selected);
+    props.setFav(!selected)
+  }
 
   return (
     <div className="photo-list__fav-icon" onClick={handleClick}>
