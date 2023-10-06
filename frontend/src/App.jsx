fa461e208804e431a18f7acccdec7375cae440cd
RomelDonovan <romeldonovan@live.ca>
2023-10-06 19:58:45 -0400

    Pass modal from app down to photolistitem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics}/>
+      <HomeRoute photos={photos} topics={topics} setModalVisible={setModalVisible} />
+      {modalVisible && (
+        <PhotoDetailsModal modalVisible={modalVisible} />
+      )}
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -8,10 +8,11 @@ const HomeRoute = (props) => {
   const photos = props.photos
   const topics = props.topics
   const [fav, setFav] = useState(false);
+
   return (
     <div className="home-route">
       <TopNavigationBar topics={topics} fav={fav}/>
-      <PhotoList photos={photos} setFav={setFav}/>
+      <PhotoList photos={photos} setFav={setFav} setModalVisible={props.setModalVisible}/>
     </div>
   );
 };
