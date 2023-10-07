e3438a6624e7ef6a276e713a3a13dc55f8e573c5
RomelDonovan <romeldonovan@live.ca>
2023-10-06 22:44:44 -0400

    Destructuring HomeRoute

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,14 +5,13 @@ import TopNavigationBar from '../components/TopNavigationBar';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = (props) => {
-  const photos = props.photos
-  const topics = props.topics
+  const {photos, topics, setModalVisible} = props
   const [fav, setFav] = useState(false);
 
   return (
     <div className="home-route">
       <TopNavigationBar topics={topics} fav={fav}/>
-      <PhotoList photos={photos} setFav={setFav} setModalVisible={props.setModalVisible}/>
+      <PhotoList photos={photos} setFav={setFav} setModalVisible={setModalVisible}/>
     </div>
   );
 };
