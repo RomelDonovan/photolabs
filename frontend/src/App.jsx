b189dee2af0b7daf38e7c6dbf4b0ee78b042b0bb
RomelDonovan <romeldonovan@live.ca>
2023-10-07 17:04:00 -0400

    Move fav state and create a togglefav function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,12 +9,23 @@ import './App.scss';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [fav, setFav] = useState([]);
+
+  const toggleFav = (photoId) => {
+    if (fav.includes(photoId)) {
+      const copyFavArr = [...fav].filter(favPhotoId => favPhotoId !== photoId)
+      setFav(copyFavArr);
+      return;
+    }
+    setFav(prev => [...prev, photoId]);
+  }
 
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} setModalVisible={setModalVisible} />
+      <HomeRoute photos={photos} topics={topics} setModalVisible={setModalVisible} fav={fav} toggleFav={toggleFav} />
+
       {modalVisible && (
-        <PhotoDetailsModal modalVisible={modalVisible} setModalVisible={setModalVisible} photos={photos}/>
+        <PhotoDetailsModal modalVisible={modalVisible} setModalVisible={setModalVisible} photos={photos} toggleFav={toggleFav} fav={fav} />
       )}
     </div>
   );
