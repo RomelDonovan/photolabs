5567c0922502db5a8031b031d0b332dc40188ea6
RomelDonovan <romeldonovan@live.ca>
2023-10-07 17:10:26 -0400

    Pass fav state and photo id to photolisttem

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,11 +3,11 @@ import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
-  const {photos, setFav, setModalVisible} = props
-  
+  const { photos, toggleFav, setModalVisible, fav } = props
+
   return (
     <ul className="photo-list">
-      {photos.map(photo => <PhotoListItem key={photo.id} {...photo} setFav={setFav} setModalVisible={setModalVisible} />)}
+      {photos.map(photo => <PhotoListItem key={photo.id} photo={photo} toggleFav={toggleFav} setModalVisible={setModalVisible} fav={fav}/>)}
     </ul>
   );
 };
