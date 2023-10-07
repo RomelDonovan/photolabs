503e921111123cfba2d38a1a48de150dcf3959d6
RomelDonovan <romeldonovan@live.ca>
2023-10-06 23:38:11 -0400

    TopicListItem destructuring

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
+  const {title} = props
+
   return (
     <div className="topic-list__item">
       <span>
-        {props.title}
+        {title}
       </span>
     </div>
   );
