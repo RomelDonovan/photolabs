a9e2bd3e3e74696b93a433763e6ea4658e94b295
RomelDonovan <romeldonovan@live.ca>
2023-10-07 17:07:57 -0400

    Pass togglefav and fav state to photolist

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
 import PhotoList from '../components/PhotoList';
 import TopNavigationBar from '../components/TopNavigationBar';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = (props) => {
-  const {photos, topics, setModalVisible} = props
-  const [fav, setFav] = useState(false);
+  const {photos, topics, setModalVisible, fav, toggleFav} = props
+  
 
   return (
     <div className="home-route">
       <TopNavigationBar topics={topics} fav={fav}/>
-      <PhotoList photos={photos} setFav={setFav} setModalVisible={setModalVisible}/>
+      <PhotoList photos={photos} toggleFav={toggleFav} setModalVisible={setModalVisible} fav={fav} />
     </div>
   );
 };
