f1207e199a80197395ccfce1ecde74acd529fac1
RomelDonovan <romeldonovan@live.ca>
2023-10-07 21:35:50 -0400

    move the logic used to manage the state

---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const useApplicationData = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [fav, setFav] = useState([]);
+
+  const toggleFav = (photoId) => {
+    if (fav.includes(photoId)) {
+      const copyFavArr = [...fav].filter(favPhotoId => favPhotoId !== photoId)
+      setFav(copyFavArr);
+      return;
+    }
+    setFav(prev => [...prev, photoId]);
+  }
+
+  return ({ state, updateToFavPhotoIds, setPhotoSelected, onClosePhotoDetailsModal })
+}
+
+export default useApplicationData;
