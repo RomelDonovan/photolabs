c112e967a2753c2d3de8f5888fc250f0384a6ec4
RomelDonovan <romeldonovan@live.ca>
2023-10-08 12:58:37 -0400

    Refactor Hook to dispatch actions

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,24 +1,53 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  // SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  // SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  CLOSE_PHOTO: 'CLOSE_PHOTO'
+  // DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
+
+// function to change that state
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FAV_PHOTO_ADDED':
+      return { ...state, fav: [...state.fav, action.payload] };
+    case 'FAV_PHOTO_REMOVED':
+      return { ...state, fav: state.fav.filter(photoId => photoId !== action.payload) };
+    case 'SELECT_PHOTO':
+      return { ...state, modalVisible: action.payload };
+    case 'CLOSE_PHOTO':
+      return { ...state, modalVisible: null };
+    default:
+      throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
+  }
+}
 
 const useApplicationData = () => {
-  const [modalVisible, setModalVisible] = useState(null);
-  const [fav, setFav] = useState([]);
+  const [state, dispatch] = useReducer(reducer, {
+    modalVisible: null,
+    fav: []
+  });
+  
+  const { modalVisible, fav } = state;
 
   const updateToFavPhotoId = (photoId) => {
-    if (fav.includes(photoId)) {
-      const copyFavArr = [...fav].filter(favPhotoId => favPhotoId !== photoId)
-      setFav(() => copyFavArr);
-      return; // Remove
+    if (state.fav.includes(photoId)) {
+      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: photoId });
+    } else {
+      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: photoId });
     }
-    setFav(prev => [...prev, photoId]); // Add
   }
 
   const setPhotoSelected = (photo) => {
-    setModalVisible(() => photo) // View
+    dispatch({ type: ACTIONS.SELECT_PHOTO, payload: photo });
   }
 
-  const onClosePhotoDetailsModal  = () => {
-    setModalVisible(false); // Close
+  const onClosePhotoDetailsModal = () => {
+    dispatch({ type: ACTIONS.CLOSE_PHOTO });
   }
 
   return ({
