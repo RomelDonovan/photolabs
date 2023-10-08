5bb4a75ec7a3bd3697fd247e1df0675e6d097919
RomelDonovan <romeldonovan@live.ca>
2023-10-08 13:02:46 -0400

    Clean up mix quotation marks to double quotations

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,25 +1,25 @@
 import { useReducer } from "react";
 
 export const ACTIONS = {
-  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
-  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
-  // SET_PHOTO_DATA: 'SET_PHOTO_DATA',
-  // SET_TOPIC_DATA: 'SET_TOPIC_DATA',
-  SELECT_PHOTO: 'SELECT_PHOTO',
-  CLOSE_PHOTO: 'CLOSE_PHOTO'
-  // DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+  FAV_PHOTO_ADDED: "FAV_PHOTO_ADDED",
+  FAV_PHOTO_REMOVED: "FAV_PHOTO_REMOVED",
+  // SET_PHOTO_DATA: "SET_PHOTO_DATA",
+  // SET_TOPIC_DATA: "SET_TOPIC_DATA",
+  SELECT_PHOTO: "SELECT_PHOTO",
+  CLOSE_PHOTO: "CLOSE_PHOTO"
+  // DISPLAY_PHOTO_DETAILS: "DISPLAY_PHOTO_DETAILS"
 }
 
 // function to change that state
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'FAV_PHOTO_ADDED':
+    case "FAV_PHOTO_ADDED":
       return { ...state, fav: [...state.fav, action.payload] };
-    case 'FAV_PHOTO_REMOVED':
+    case "FAV_PHOTO_REMOVED":
       return { ...state, fav: state.fav.filter(photoId => photoId !== action.payload) };
-    case 'SELECT_PHOTO':
+    case "SELECT_PHOTO":
       return { ...state, modalVisible: action.payload };
-    case 'CLOSE_PHOTO':
+    case "CLOSE_PHOTO":
       return { ...state, modalVisible: null };
     default:
       throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
