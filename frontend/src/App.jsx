ed648624c2e7368579882e973e2086fd7dfef75b
RomelDonovan <romeldonovan@live.ca>
2023-10-07 21:34:36 -0400

    Copy comment out and place in useApplicationData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { useState } from 'react';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [fav, setFav] = useState([]);
+//   const [modalVisible, setModalVisible] = useState(false);
+//   const [fav, setFav] = useState([]);
 
-  const toggleFav = (photoId) => {
-    if (fav.includes(photoId)) {
-      const copyFavArr = [...fav].filter(favPhotoId => favPhotoId !== photoId)
-      setFav(copyFavArr);
-      return;
-    }
-    setFav(prev => [...prev, photoId]);
-  }
+//   const toggleFav = (photoId) => {
+//     if (fav.includes(photoId)) {
+//       const copyFavArr = [...fav].filter(favPhotoId => favPhotoId !== photoId)
+//       setFav(copyFavArr);
+//       return;
+//     }
+//     setFav(prev => [...prev, photoId]);
+//   }
 
   return (
     <div className="App">
