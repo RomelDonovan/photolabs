775b82b5f2aa1a067d6e1e057ea9dfb7b2d84597
RomelDonovan <romeldonovan@live.ca>
2023-10-07 22:35:17 -0400

    Import useApplicationdata into App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import useApplicationData from 'hooks/useApplicationData';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 import './App.scss';
 
 const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [fav, setFav] = useState([]);
+  const {
+    state: { modalVisible, fav },
+    setModalVisible,
+    toggleFav
+  } = useApplicationData();
 
-  const toggleFav = (photoId) => {
-    if (fav.includes(photoId)) {
-      const copyFavArr = [...fav].filter(favPhotoId => favPhotoId !== photoId)
-      setFav(copyFavArr);
-      return;
-    }
-    setFav(prev => [...prev, photoId]);
-  }
+
+  // const [modalVisible, setModalVisible] = useState(false);
+  // const [fav, setFav] = useState([]);
+
+  // const toggleFav = (photoId) => {
+  //   if (fav.includes(photoId)) {
+  //     const copyFavArr = [...fav].filter(favPhotoId => favPhotoId !== photoId)
+  //     setFav(copyFavArr);
+  //     return;
+  //   }
+  //   setFav(prev => [...prev, photoId]);
+  // }
 
   return (
     <div className="App">
