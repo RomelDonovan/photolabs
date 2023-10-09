6063ce50c29060082eb1f907e3aa63c19b53be0d
RomelDonovan <romeldonovan@live.ca>
2023-10-09 13:45:38 -0400

    Pass photosByTopic with topic id func with onClick

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
-  const {title} = props
+  const {title, photosByTopic} = props
 
   return (
-    <div className="topic-list__item">
+    <div className="topic-list__item" onClick={photosByTopic}>
       <span>
         {title}
       </span>
